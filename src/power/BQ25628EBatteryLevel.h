b22518127fcc469f78d919fd086ea1823ac7a585
완두콩 <git@wdk.kr>
2026-08-21 15:30:34 +0900

    Apply repository clang format

--- src/power/BQ25628EBatteryLevel.h
+++ src/power/BQ25628EBatteryLevel.h
@@ -4,34 +4,23 @@
 
 #ifdef HAS_BQ25628E
 
-template <typename BatteryLevelBase>
-class BQ25628EBatteryLevel final : public BatteryLevelBase {
-public:
-  int getBatteryPercent() override { return -1; }
+template <typename BatteryLevelBase> class BQ25628EBatteryLevel final : public BatteryLevelBase
+{
+  public:
+    int getBatteryPercent() override { return -1; }
 
-  uint16_t getBattVoltage() override {
-    return hasValidMeasurement() ? bq25628e->measurements().batteryVoltageMv
-                                 : 0;
-  }
+    uint16_t getBattVoltage() override { return hasValidMeasurement() ? bq25628e->measurements().batteryVoltageMv : 0; }
 
-  bool isBatteryConnect() override {
-    return hasValidMeasurement() && !bq25628e->hasInput();
-  }
+    bool isBatteryConnect() override { return hasValidMeasurement() && !bq25628e->hasInput(); }
 
-  bool isVbusIn() override { return hasValidStatus() && bq25628e->hasInput(); }
+    bool isVbusIn() override { return hasValidStatus() && bq25628e->hasInput(); }
 
-  bool isCharging() override {
-    return hasValidStatus() && bq25628e->isCharging();
-  }
+    bool isCharging() override { return hasValidStatus() && bq25628e->isCharging(); }
 
-private:
-  bool hasValidStatus() const {
-    return bq25628e != nullptr && bq25628e->lastStatusReadSucceeded();
-  }
+  private:
+    bool hasValidStatus() const { return bq25628e != nullptr && bq25628e->lastStatusReadSucceeded(); }
 
-  bool hasValidMeasurement() const {
-    return hasValidStatus() && bq25628e->measurements().valid;
-  }
+    bool hasValidMeasurement() const { return hasValidStatus() && bq25628e->measurements().valid; }
 };
 
 #endif // HAS_BQ25628E
